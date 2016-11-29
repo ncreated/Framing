cbd7c3929efd20c1dcb181208c95414ce75d101a
ncreated <ncreated@gmail.com>
2016-11-29 22:20:24 +0100

    Initial commit

---
+++ Framing/Framing.h
@@ -0,0 +1,19 @@
+//
+//  Framing.h
+//  Framing
+//
+//  Created by Maciek Grzybowski on 29.11.2016.
+//  Copyright © 2016 ncreated. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Framing.
+FOUNDATION_EXPORT double FramingVersionNumber;
+
+//! Project version string for Framing.
+FOUNDATION_EXPORT const unsigned char FramingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Framing/PublicHeader.h>
+
+
